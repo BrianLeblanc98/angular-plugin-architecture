8bdb77bcc760af658637ef5f66b23b0986a46f1d
brian.leblanc <brian.leblanc@caseware.com>
2019-05-23 11:16:20 -0400

    Remove SystemJS, use webpack to import module instead

--- plugins/plugin-a/src/app/plugin-a.module.ts
+++ plugins/plugin-a/src/app/plugin-a.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PluginAComponent } from './plugin-a.component';
 import { FormsModule } from '@angular/forms';
+import { PluginAComponent } from './plugin-a.component';
 
 @NgModule({
   imports: [
